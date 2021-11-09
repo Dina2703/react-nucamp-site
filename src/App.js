bdc6772003a0908f05bb015414312daf9d480761
Dinara Idrissova <Dinara.Idrissova@list.ru>
2021-11-08 19:49:44 -0500

    Components Part 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Directory from './components/DirectoryComponent';
 import './App.css';
+
 
 
 class App extends Component {
@@ -12,6 +14,7 @@ class App extends Component {
                   <NavbarBrand href="/">NuCamp</NavbarBrand>
               </div>
               </Navbar>
+              <Directory />
           </div>
       );
   }
