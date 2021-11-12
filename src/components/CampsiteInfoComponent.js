bdad0ef282aeb6abd3a277f516fb4a74c4ec6342
Dinara Idrissova <Dinara.Idrissova@list.ru>
2021-11-12 07:46:14 -0500

    test-2

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,9 +1,27 @@
 import React, { Component } from 'react';
+import { Card, CardImg, CardText, CardTitle, CardBody } from 'reactstrap';
+
 
 class CamsiteInfo extends Component {
+    renderCampsite(camsite) {
+        return (
+            <div className="col-md-5 m-1" >
+            <Card>
+                <CardImg top src={this.props.campsite.image} alt={this.props.campsite.name} />
+                <CardBody>
+                    <CardTitle>{this.props.campsite.name}</CardTitle>
+                    <CardText>{this.props.campsite.description}</CardText>
+                </CardBody>
+            </Card>
+        </div>
+        );
+    }
+
     render() { 
         if(this.props.campsite) {
-            return <div className="row"></div>;
+            return <div className="row">
+                {this.renderCampsite(this.props.campsite)}
+            </div>;
         }
         return <div></div>;
     }
