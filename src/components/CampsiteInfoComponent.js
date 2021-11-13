1492e211d21367638e156da4bc08fb35da51fdff
Dinara Idrissova <Dinara.Idrissova@list.ru>
2021-11-13 11:45:07 -0500

    Workshop Assignment.task2

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
+import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 
 class  CampsiteInfo  extends Component {
+    renderCampsite(camsite) {
+        return (
+            <div className="col-md-5 m-1" >
+            <Card>
+            <CardImg top src={this.props.campsite.image} alt={this.props.campsite.name} />
+            <CardBody>
+                <CardTitle>{this.props.campsite.name}</CardTitle>
+                <CardText>{this.props.campsite.description}</CardText>
+            </CardBody>
+        </Card>
+            </div>
+        );
+    }
+
     render() { 
         if(this.props.campsite) {
             return (
-                <div className="row" ></div>
+                <div className="row" >
+                {this.renderCampsite(this.props.campsite)}
+                </div>
             );
         }
         return <div></div>;
@@ -12,3 +29,4 @@ class  CampsiteInfo  extends Component {
 }
  
 export default  CampsiteInfo ;
+
