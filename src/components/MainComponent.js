6b3fbc7296bb50103d9c03d6a561a0d3dc18232d
Dinara Idrissova <Dinara.Idrissova@list.ru>
2021-11-17 10:04:03 -0500

    update shared file

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -29,7 +29,7 @@ class Main extends Component {
         <Switch>
           <Route path='/home' component={HomePage} />
           <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites}/>} />
-          <Route exact='/contactus' component={ Contact } />
+          <Route exact path='/contactus' component={ Contact } />
           <Redirect to='/home' />
         </Switch>
         <Footer />
