26007cd659caca9607a16ddf2e2814f9b4738b73
Dinara Idrissova <Dinara.Idrissova@list.ru>
2021-11-23 10:49:17 -0500

    add modal form

--- src/components/HeaderComponent.js
+++ src/components/HeaderComponent.js
@@ -1,33 +1,55 @@
-import React, { Component } from 'react';
-import { Nav, Navbar, NavbarBrand, NavbarToggler, Collapse, NavItem, Jumbotron, Button, Modal, ModalHeader, ModalBody } from 'reactstrap';
-import { NavLink } from 'react-router-dom';
+import React, { Component } from "react";
+import {
+  Nav,
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  Collapse,
+  NavItem,
+  Jumbotron,
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  Form,
+  FormGroup,
+  Input,
+  Label,
+} from "reactstrap";
+import { NavLink } from "react-router-dom";
 
 class Header extends Component {
   constructor(props) {
     super(props);
 
-
     this.state = {
       isNavOpen: false,
-      isModalOpen: false
+      isModalOpen: false,
     };
 
     this.toggleNav = this.toggleNav.bind(this);
     this.toggleModal = this.toggleModal.bind(this);
+    this.handleLogin = this.handleLogin.bind(this);
   }
 
-  toggleNav(){
-    this.setState({ 
-      isNavOpen: !this.state.isNavOpen
+  toggleNav() {
+    this.setState({
+      isNavOpen: !this.state.isNavOpen,
     });
   }
-  toggleModal(){
-    this.setState({ 
-      isModalOpen: !this.state.isModalOpen
+  toggleModal() {
+    this.setState({
+      isModalOpen: !this.state.isModalOpen,
     });
   }
 
-  render() { 
+  handleLogin(event){
+    alert(`Username: ${this.username.value} Password: ${this.password.value} Remember: ${this.remember.checked}`);
+    this.toggleModal();
+    event.preventDefault();
+  }
+
+  render() {
     return (
       <React.Fragment>
         <Jumbotron fluid>
@@ -42,30 +64,37 @@ class Header extends Component {
         </Jumbotron>
         <Navbar dark sticky="top" expand="md">
           <div className="container">
-            <NavbarBrand className="mr-auto" href="/"><img src="/assets/images/logo.png" height="30" width="30" alt="Nucamp Logo" /></NavbarBrand>
+            <NavbarBrand className="mr-auto" href="/">
+              <img
+                src="/assets/images/logo.png"
+                height="30"
+                width="30"
+                alt="Nucamp Logo"
+              />
+            </NavbarBrand>
             <NavbarToggler onClick={this.toggleNav} />
-            <Collapse isOpen={ this.state.isNavOpen } navbar>
+            <Collapse isOpen={this.state.isNavOpen} navbar>
               <Nav navbar>
                 <NavItem>
-                  <NavLink className="nav-link" to= "/home">
+                  <NavLink className="nav-link" to="/home">
                     <i className="fa fa-home fa-lg" /> Home
                   </NavLink>
                 </NavItem>
                 <NavItem>
-                <NavLink className="nav-link" to= "/directory">
-                  <i className="fa fa-list fa-lg" /> Directory
-                </NavLink>
-              </NavItem>
-              <NavItem>
-              <NavLink className="nav-link" to= "/aboutus">
-                <i className="fa fa-info fa-lg" /> About
-              </NavLink>
-            </NavItem>
-            <NavItem>
-            <NavLink className="nav-link" to= "/contactus">
-              <i className="fa fa-address-card fa-lg" /> Contact Us
-            </NavLink>
-          </NavItem>
+                  <NavLink className="nav-link" to="/directory">
+                    <i className="fa fa-list fa-lg" /> Directory
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink className="nav-link" to="/aboutus">
+                    <i className="fa fa-info fa-lg" /> About
+                  </NavLink>
+                </NavItem>
+                <NavItem>
+                  <NavLink className="nav-link" to="/contactus">
+                    <i className="fa fa-address-card fa-lg" /> Contact Us
+                  </NavLink>
+                </NavItem>
               </Nav>
               <span className="navbar-text ml-auto">
                 <Button outline onClick={this.toggleModal}>
@@ -79,12 +108,25 @@ class Header extends Component {
         <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
           <ModalHeader toggle={this.toggleModal}>Login</ModalHeader>
           <ModalBody>
-
-
+            <Form onSubmit={this.handleLogin}>
+              <FormGroup>
+                <Label htmlFor="username">Username</Label>
+                <Input type="text" id="username" name="username" />
+              </FormGroup>
+              <FormGroup>
+                <Label htmlFor="password">Password</Label>
+                <Input type="password" id="password" name="password" />
+              </FormGroup>
+              <FormGroup check>
+                <Input type="checkbox" name="remember" />
+                Remember me
+              </FormGroup>
+              <Button type="submit" value="sumbit" color="primary">Login</Button>
+            </Form>
           </ModalBody>
         </Modal>
       </React.Fragment>
     );
-}
+  }
 }
 export default Header;
