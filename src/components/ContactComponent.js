33d0f89cc5f58025ce73f0f6232bc6b6aa884328
Dinara Idrissova <Dinara.Idrissova@list.ru>
2021-11-25 10:27:01 -0500

    Validation logic

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -8,7 +8,13 @@ import {
   Row
 } from "reactstrap";
 import { Link } from "react-router-dom";
-import { Control, LocalForm } from "react-redux-form";
+import { Control, LocalForm, Errors } from "react-redux-form";
+
+const required = val => val && val.length;
+const maxLength = len => val => !val || (val.length <= len);
+const minLength = len => val => val && (val.length >= len);
+const isNumber = val => !isNaN(+val);
+const validEmail = val => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val);
 
 class Contact extends React.Component {
   constructor(props) {
